98fa316c74e62ff4b1dac03c5ffe9a6f973f9b0d
hadehh34 <ilhamfadhilah30@gmail.com>
2024-05-14 09:53:56 +0700

    Add files via upload

---
+++ Photograpi2/data/story.js
@@ -0,0 +1,915 @@
+var story = {
+ "docName": "Photograpi",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "Photograpi",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "20:899",
+   "groupIndex": 0,
+   "title": "logo resvara 1",
+   "image": "logo-resvara-1.png",
+   "index": 0,
+   "width": 225,
+   "height": 225,
+   "x": -39604,
+   "y": 11705,
+   "isFrame": false,
+   "type": "item",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "4:5687",
+   "groupIndex": 0,
+   "title": "Landing Page",
+   "image": "landing-page.png",
+   "index": 1,
+   "width": 1440,
+   "height": 8682,
+   "x": -24533,
+   "y": 8652,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Pricing",
+     "rect": {
+      "x": 711,
+      "y": 48,
+      "width": 46,
+      "height": 16
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Service",
+     "rect": {
+      "x": 990,
+      "y": 48,
+      "width": 80,
+      "height": 16
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Project",
+     "rect": {
+      "x": 1134,
+      "y": 48,
+      "width": 78,
+      "height": 16
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Contact",
+     "rect": {
+      "x": 1276,
+      "y": 48,
+      "width": 84,
+      "height": 16
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 524,
+      "y": 4325,
+      "width": 391,
+      "height": 54
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 1239,
+      "y": 4634,
+      "width": 121,
+      "height": 48
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 788,
+      "y": 6260,
+      "width": 547,
+      "height": 57
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 564,
+      "y": 8057,
+      "width": 312,
+      "height": 51
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "12:106",
+   "groupIndex": 0,
+   "title": "Our Service",
+   "image": "our-service.png",
+   "index": 2,
+   "width": 1440,
+   "height": 5987,
+   "x": -22991,
+   "y": 8652,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Pricing",
+     "rect": {
+      "x": 711,
+      "y": 48,
+      "width": 46,
+      "height": 16
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Project",
+     "rect": {
+      "x": 1134,
+      "y": 48,
+      "width": 78,
+      "height": 16
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Contact",
+     "rect": {
+      "x": 1276,
+      "y": 48,
+      "width": 84,
+      "height": 16
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 564,
+      "y": 5362,
+      "width": 312,
+      "height": 51
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 744,
+      "y": 1436,
+      "width": 547,
+      "height": 57
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 148,
+      "y": 2221,
+      "width": 547,
+      "height": 57
+     },
+     "index": 13,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 744,
+      "y": 3060,
+      "width": 547,
+      "height": 57
+     },
+     "index": 14,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 148,
+      "y": 3845,
+      "width": 547,
+      "height": 57
+     },
+     "index": 15,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 744,
+      "y": 4684,
+      "width": 547,
+      "height": 57
+     },
+     "index": 16,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "12:1305",
+   "groupIndex": 0,
+   "title": "Our Priicing",
+   "image": "our-priicing.png",
+   "index": 3,
+   "width": 1440,
+   "height": 6967,
+   "x": -21438,
+   "y": 8652,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Our Service",
+     "rect": {
+      "x": 990,
+      "y": 48,
+      "width": 80,
+      "height": 16
+     },
+     "index": 17,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Project",
+     "rect": {
+      "x": 1134,
+      "y": 48,
+      "width": 78,
+      "height": 16
+     },
+     "index": 18,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Contact",
+     "rect": {
+      "x": 1276,
+      "y": 48,
+      "width": 84,
+      "height": 16
+     },
+     "index": 19,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 541.5,
+      "y": 1626,
+      "width": 356,
+      "height": 60
+     },
+     "index": 20,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 541.5,
+      "y": 2650,
+      "width": 356,
+      "height": 60
+     },
+     "index": 21,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 237.5,
+      "y": 3650,
+      "width": 356,
+      "height": 60
+     },
+     "index": 22,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 795.5,
+      "y": 3695,
+      "width": 356,
+      "height": 60
+     },
+     "index": 23,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 237.5,
+      "y": 5698,
+      "width": 356,
+      "height": 60
+     },
+     "index": 24,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 795.5,
+      "y": 5743,
+      "width": 356,
+      "height": 60
+     },
+     "index": 25,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 178,
+      "y": 4678.1904296875,
+      "width": 268,
+      "height": 60
+     },
+     "index": 26,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 585,
+      "y": 4678.1904296875,
+      "width": 268,
+      "height": 60
+     },
+     "index": 27,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 1024,
+      "y": 4678.1904296875,
+      "width": 268,
+      "height": 60
+     },
+     "index": 28,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 564,
+      "y": 6342,
+      "width": 312,
+      "height": 51
+     },
+     "index": 29,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "23:56",
+   "groupIndex": 0,
+   "title": "Our Project",
+   "image": "our-project.png",
+   "index": 4,
+   "width": 1440,
+   "height": 12244,
+   "x": -19884,
+   "y": 8652,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Pricing",
+     "rect": {
+      "x": 711,
+      "y": 48,
+      "width": 46,
+      "height": 16
+     },
+     "index": 30,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Service",
+     "rect": {
+      "x": 990,
+      "y": 48,
+      "width": 80,
+      "height": 16
+     },
+     "index": 31,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Contact",
+     "rect": {
+      "x": 1276,
+      "y": 48,
+      "width": 84,
+      "height": 16
+     },
+     "index": 32,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 564,
+      "y": 11619,
+      "width": 312,
+      "height": 51
+     },
+     "index": 33,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "28:1832",
+   "groupIndex": 0,
+   "title": "Contact us",
+   "image": "contact-us.png",
+   "index": 5,
+   "width": 1440,
+   "height": 1400,
+   "x": -18283,
+   "y": 8652,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Pricing",
+     "rect": {
+      "x": 711,
+      "y": 48,
+      "width": 46,
+      "height": 16
+     },
+     "index": 34,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Service",
+     "rect": {
+      "x": 990,
+      "y": 48,
+      "width": 80,
+      "height": 16
+     },
+     "index": 35,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Our Project",
+     "rect": {
+      "x": 1134,
+      "y": 48,
+      "width": 78,
+      "height": 16
+     },
+     "index": 36,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  }
+ ],
+ "startPageIndex": 1,
+ "totalImages": 6,
+ "fileKey": "Photograpi",
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#F5F5F5"
+  }
+ ]
+}
